3eba6b771fdff7e325bd20b155686f5231a3ac36
Mirko Bellabarba <mirko.bellabarba@elfo.net>
2022-11-26 16:05:30 +0100

    refactoring and creation of routes

---
+++ pages/detail/[[id]].vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    Detail {{ idQuake }}
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { isNil } from 'lodash';
+
+const route = useRoute();
+
+const idQuake = computed(() => {
+  if (isNil(route.params.id))
+    return 'null';
+  return route.params.id;
+})
+</script>

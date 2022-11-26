71859b3f49c4867c6ac95a7fc3f82b10bf6a9097
Mirko Bellabarba <mirko.bellabarba@elfo.net>
2022-11-26 17:10:06 +0100

    Revert "added tailwindCSS"
    This reverts commit bf55ee1c6d72e79b3e77ffd7a3b41c21690882ae.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  buildModules: ['@nuxtjs/tailwindcss'],
-  
   typescript: {
     typeCheck: true
-  },
-
-  css: [
-    '@/assets/css/tailwind.css',
-  ],
+  }
 })
